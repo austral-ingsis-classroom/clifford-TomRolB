3d0f98c21d4a42cce0af6e200f77bf403093c0e7
Tomás <tomasrolb@gmail.com>
2024-06-11 15:59:38 -0300

    Checked corner cases at Remove

--- src/main/java/edu/austral/ingsis/clifford/Remove.java
+++ src/main/java/edu/austral/ingsis/clifford/Remove.java
@@ -9,15 +9,29 @@ public class Remove implements Action {
         this.arguments = arguments;
     }
 
+    //TODO: should probably turn it into a Visitor, to avoid type checking
+    //TODO: modularize
     @Override
     public String execute() {
+        if (arguments.length == 1) return "Invalid command: no arguments were passed";
+
         if (arguments[1].equals("--recursive")) {
+            if (arguments.length != 2) {
+                return "Invalid number of arguments: must pass the directory to be deleted";
+            }
+
             fs.getWorkingDirectory().removeChild(arguments[2]);
             return "'" + arguments[2] + "' removed";
         }
         else {
+            if (tryingToRemoveDirectory()) return "cannot remove '" + arguments[2] + "', is a directory";
+
             fs.getWorkingDirectory().removeChild(arguments[1]);
             return "'" + arguments[1] + "' removed";
         }
+    }
+
+    private boolean tryingToRemoveDirectory() {
+        return fs.getWorkingDirectory().getChild(arguments[1]) instanceof Directory;
     }
 }
